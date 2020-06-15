3aecfa0752d14be160649a76035fce8361b75d0a
PedrassoxD <pedroperlab@gmail.com>
2020-06-15 17:03:59 +0200

    New Update

--- MeetingMinutesFrontend/PFPedro/src/app/app.component.ts
+++ MeetingMinutesFrontend/PFPedro/src/app/app.component.ts
@@ -13,12 +13,21 @@ export class AppComponent {
   constructor(public us: UsuarioService,
               private router: Router) { }
 
+  policy = true;
 
   /** Método para cerrar sesión en la página */
 
   logout() {
     this.us.logOut();
     this.router.navigate(['login']);
+  }
+
+  aceptarNoAceptar() {
+    this.policy = false;
+  }
+
+  leerMas() {
+    window.open('../assets/pdf/Policy_MM.pdf' + '#page=' + 1, '_blank', '', true);
   }
 
 }
